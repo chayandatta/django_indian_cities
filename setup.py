79b16501183422a99c7919e1db26dcd0782d3672
Chayan <chayan.d@mantralabsglobal.com>
2020-08-24 18:44:34 +0530

    prerelease

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="django-indian-cities",
+    version="0.0.1",
+    description="Django Indian Cities",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    py_modules=["dj-city"],
+    package_dir={"": "src"},
+    url="https://github.com/chayandatta/django_indian_cities",
+    author="Chayan Datta",
+    author_email="chayan.datta1996@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+)
--- test.py
+++ test.py
@@ -1,3 +1,3 @@
-from django_indian_cities.dj_city import CITY_STATE_CHOICES
+from src.dj_city import CITY_STATE_CHOICES
 
 print(CITY_STATE_CHOICES[0][1])

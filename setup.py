afa9ef51e48259e439cf57a2178a7b66d2e199f4
Chayan <chayan.datta1996@gmail.com>
2020-08-24 20:14:42 +0530

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="indian_cities",
-    version="0.0.2",
+    version="0.0.3",
     description="Django Indian Cities",
     long_description=long_description,
     long_description_content_type="text/markdown",
